c9f1dd01ba3133ceb4d50c609c9100f333ca6b6b
andrei andrei <andrei@joyspay.com>
2025-07-20 22:10:28 +0300

    chore: move setupTests file

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
+    setupFiles: './src/__tests__/setupTests.ts',
     coverage: {
       provider: 'v8',
       include: ['src/**/*.{js,jsx,ts,tsx}'],
