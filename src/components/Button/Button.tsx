81c61b2b196b71af9d3baa107432fadab220363a
andrei andrei <andrei@joyspay.com>
2025-07-22 22:09:13 +0300

    refactor: convert Button component from class to functional

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,26 +1,21 @@
-import { Component, type MouseEvent } from 'react';
 import styles from './button.module.css';
 
 type ButtonProps = {
   label: string;
   maxWith?: string;
-  onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
   disabled?: boolean;
 };
 
-export class Button extends Component<ButtonProps> {
-  render() {
-    const { label, onClick, disabled } = this.props;
-
-    return (
-      <button
-        className={styles.button}
-        onClick={onClick}
-        disabled={disabled}
-        style={{ maxWidth: this.props.maxWith ? this.props.maxWith : 'unset' }}
-      >
-        {label}
-      </button>
-    );
-  }
-}
+export const Button = ({ label, maxWith, onClick, disabled }: ButtonProps) => {
+  return (
+    <button
+      className={styles.button}
+      onClick={onClick}
+      disabled={disabled}
+      style={{ maxWidth: maxWith ? maxWith : 'unset' }}
+    >
+      {label}
+    </button>
+  );
+};
