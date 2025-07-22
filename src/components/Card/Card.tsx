766ea0da4f84640bce259a8e130dd18eb5af470a
andrei andrei <andrei@joyspay.com>
2025-07-22 22:12:14 +0300

    refactor: convert Card component from class to functional

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import styles from './card.module.css';
 import ReactLogo from '../../assets/react.svg';
 
@@ -9,29 +8,25 @@ type CardProps = {
   gender: string;
 };
 
-export class Card extends Component<CardProps> {
-  render() {
-    const { image, name, locationName, gender } = this.props;
-
-    return (
-      <div className={styles.card}>
-        <img
-          className={styles.image}
-          src={image ? image : ReactLogo}
-          alt={name ? name : 'Avatar'}
-        />
-        <div>
-          <div className={styles.name} data-testid="name">
-            {name ? name : 'Name is empty'}
-          </div>
-          <div className={styles.location} data-testid="location">
-            {locationName ? locationName : 'Location is empty'}
-          </div>
-          <div className={styles.gender} data-testid="gender">
-            {gender ? gender : 'Gender is empty'}
-          </div>
+export const Card = ({ image, name, locationName, gender }: CardProps) => {
+  return (
+    <div className={styles.card}>
+      <img
+        className={styles.image}
+        src={image ? image : ReactLogo}
+        alt={name ? name : 'Avatar'}
+      />
+      <div>
+        <div className={styles.name} data-testid="name">
+          {name ? name : 'Name is empty'}
+        </div>
+        <div className={styles.location} data-testid="location">
+          {locationName ? locationName : 'Location is empty'}
+        </div>
+        <div className={styles.gender} data-testid="gender">
+          {gender ? gender : 'Gender is empty'}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
